7fee0e54300b96ed0557485e22b1d5e9877ab359
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2025-01-10 16:10:10 -0800

    fix(ci): WAVM download script

--- scripts/download-machine.sh
+++ scripts/download-machine.sh
@@ -5,7 +5,12 @@ mkdir "$2"
 ln -sfT "$2" latest
 cd "$2"
 echo "$2" > module-root.txt
-url_base="https://github.com/OffchainLabs/nitro/releases/download/$1"
+if [ -z "$3" ]; then
+    url_base="https://github.com/OffchainLabs/nitro/releases/download/$1"
+else
+    url_base="https://github.com/$3/nitro/releases/download/$1"
+fi
+
 wget "$url_base/machine.wavm.br"
 
 status_code="$(curl -LI "$url_base/replay.wasm" -so /dev/null -w '%{http_code}')"
